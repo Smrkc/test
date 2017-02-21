c340d626b91ba36b7eb4b9b6227c8405a07da529
Nejc Kozjek <kozjek@gmail.com>
2017-02-21 15:58:42 +0100

    Changed qsort

--- qsort.py
+++ qsort.py
@@ -1,2 +1,4 @@
+#!/usr/bin/python
+
 qsort = lambda l : l if len(l)<=1 else qsort([x for x in l[1:] if x < l[0]]) + [l[0]] + qsort([x for x in l[1:] if x >= l[0]])
-qsort([1,2,5,7,8,2,6,8])
+print(qsort([1,2,5,7,8,2,6,8,13,11,88,45,12]))
